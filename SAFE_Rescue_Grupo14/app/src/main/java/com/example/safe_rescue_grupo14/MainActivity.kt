338b74b139e510dce54b7a75641cb33af0507f85
Mauricio Tapia Ayala <ma.tapia.ay@gmail.com>
2025-09-16 15:04:26 -0300

    1.0
    - Inicialización del repositorio
    - Se agregaron los recursos iniciales, incluidas las carpetas de organización

---
+++ SAFE_Rescue_Grupo14/app/src/main/java/com/example/safe_rescue_grupo14/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.safe_rescue_grupo14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.safe_rescue_grupo14.ui.theme.SAFE_Rescue_Grupo14Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SAFE_Rescue_Grupo14Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SAFE_Rescue_Grupo14Theme {
+        Greeting("Android")
+    }
+}
